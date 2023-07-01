3cb3a8b85365889d794e27f85b808f24719f6f57
acemccloud1 <acemccloud1@gmail.com>
2023-07-01 10:43:41 +0530

    Added AppInfo frame; Cosmetic changes

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ class Window():
         self.master = master
         self.master.title("PDF - Extract & Merge")
         self.master.iconbitmap(fr"Z:\Python_Projects\PDF_Editor\python.ico")
-        self.master.geometry('650x250')
+        self.master.geometry('700x300')
 
         self.RadioFrame = LabelFrame(self.master, text="Functions", width=250, height=150)
         self.RadioFrame.place(x=10, y=10)
+
+        self.InfoFrame = LabelFrame(self.master, text="AppInfo", width=250, height=150)
+        self.InfoFrame.place(x=10, y=220)
 
         Options = {
             "Extract All": "extractall",
@@ -33,11 +36,14 @@ class Window():
         for text, value in Options.items():
             Radiobutton(self.RadioFrame, text=text, variable=selection, value=value, command=lambda: Clicked(selection.get())).pack(anchor=W)
 
+        Label(self.InfoFrame, text=f"Creator:- Unmesh Patil\nLanguage: Python\nVersion: 1.0.0", wraplength=400, justify=LEFT).pack(anchor=W)
+
         def Clicked(variable):
             if selection.get() == "extractall":
                 self.ExtractAllFrame = LabelFrame(self.master, text="Extract All Pages", width=250, height=150)
-                self.ExtractAllFrame.place(x=125, y=10)
+                self.ExtractAllFrame.place(x=165, y=10)
                 frame = self.ExtractAllFrame
+                Label(frame, text="NOTE:- You may select only one file at a time for extraction.", fg='OrangeRed', wraplength=400, justify=LEFT).grid(row=1, column=0, columnspan=2)
                 def extractall():
                     try:
                         # Fetch the filepath
@@ -60,7 +66,7 @@ class Window():
                         messagebox.showinfo(title="Success", message="Task completed successfully.")
                     except Exception as e:
                         messagebox.showerror(title="ERROR", message=fr"Error:- {e}")
-                Button(frame, text="Select File", command=lambda: extractall()).grid(row=2, column=1, padx=5, pady=5)
+                Button(frame, text="Select File", command=lambda: extractall()).grid(row=2, column=0, padx=10, pady=10, columnspan=2)
                 try:
                     self.ExtractRangeFrame.destroy()
                     self.MergeFrame.destroy()
@@ -69,7 +75,7 @@ class Window():
 
             elif selection.get() == "extractrange":
                 self.ExtractRangeFrame = LabelFrame(self.master, text="Extract Custom Range", width=250, height=150)
-                self.ExtractRangeFrame.place(x=125, y=10)
+                self.ExtractRangeFrame.place(x=165, y=10)
                 frame = self.ExtractRangeFrame
                 Label(frame, text="Step# 1 --> Start Page#", wraplength=400, justify=LEFT).grid(row=0, column=0)
                 e1 = Entry(frame)
@@ -114,9 +120,10 @@ class Window():
 
             elif selection.get() == "merge":
                 self.MergeFrame = LabelFrame(self.master, text="Merge PDF", width=250, height=150)
-                self.MergeFrame.place(x=125, y=10)
+                self.MergeFrame.place(x=165, y=10)
                 frame = self.MergeFrame
-                Label(frame, text="NOTE:- The selected files will be sorted in an ascending oder before merging, so please name them in the order you want them merged.", fg='red', wraplength=400, justify=LEFT).grid(row=1, column=0, columnspan=2)
+                Label(frame, text="NOTE:- The selected files will be sorted in an ascending oder before merging, so please name them in the order you want them merged.", fg='OrangeRed', wraplength=475, justify=LEFT).grid(row=1, column=0, columnspan=2, sticky='w')
+                Label(frame, text="TIP:- Prefix the filenames with numericals (1, 2 etc.) to enforce the order sequence.", fg='MediumSeaGreen', wraplength=475, justify=LEFT).grid(row=2, column=0, columnspan=2, sticky='w')
                 def mergeall():
                     try:
                         # Fetch the folderpath
@@ -124,7 +131,7 @@ class Window():
                         filepath = filepath_tuple[0].replace('/', '\\')
                         p = Path(filepath)
                         outputdir = str(p.parent)
-                        Label(frame, text=f"Output Directory:- {outputdir}", wraplength=400, justify=LEFT).grid(row=6, column=0, sticky='w')
+                        Label(frame, text=f"Output Directory:- {outputdir}", wraplength=475, justify=LEFT).grid(row=6, column=0, sticky='w')
                         # Merge all the PDF files in the directory
                         output = PdfMerger()
                         for i, filepath in enumerate(filepath_tuple):
@@ -134,11 +141,11 @@ class Window():
                         merged_filepath = fr"{outputdir}\Merged.pdf"
                         with open(merged_filepath, "wb") as output_stream:
                             output.write(output_stream)
-                        Label(frame, text=f"Merged File:- {merged_filepath}", bg="lime", fg="black", wraplength=400, justify=LEFT).grid(row=7, column=0, sticky='w')
+                        Label(frame, text=f"Merged File:- {merged_filepath}", bg="lime", fg="black", wraplength=475, justify=LEFT).grid(row=7, column=0, sticky='w')
                         messagebox.showinfo(title="Success", message="Task completed successfully.")
                     except Exception as e:
                         messagebox.showerror(title="ERROR", message=fr"Error:- {e}")
-                Button(frame, text="Select Files", command=lambda: mergeall()).grid(row=2, column=0, padx=10, pady=10, columnspan=2)
+                Button(frame, text="Select Files", command=lambda: mergeall()).grid(row=3, column=0, padx=10, pady=10, columnspan=2)
                 try:
                     self.ExtractAllFrame.destroy()
                     self.ExtractRangeFrame.destroy()
